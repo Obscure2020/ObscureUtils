5b069498264f604f7881fc67204b3521c84a9110
Obscure2020 <jas.elgoog@gmail.com>
2022-07-25 15:47:59 -0400

    MP3 YTD now preserves order.

--- Windows/MP3 YTD.bat
+++ Windows/MP3 YTD.bat
@@ -18,19 +18,20 @@ if not "%input%" equ "" goto loop
 rmdir /s /q audios
 mkdir audios
 cd audios
+mkdir outputs
 for /f "tokens=*" %%u in (..\listing.txt) do (
     yt-dlp -N 4 -S acodec -f "bestaudio/bestaudio*" -R 30 --file-access-retries 30 --fragment-retries 30 --merge-output-format mkv "%%u"
-)
-yt-dlp --rm-cache-dir
-mkdir outputs
-cd outputs
-for %%u in (..\*) do (
-    ffmpeg -hide_banner -i "..\%%~nxu" -c:a pcm_f32le "%%~nu".wav
-)
-for %%u in (*.wav) do (
-    lame -q 0 -b 320 --cbr --replaygain-accurate "%%~nxu"
+    cd outputs
+    for %%j in (..\*) do (
+        ffmpeg -hide_banner -i "..\%%~nxj" -map_metadata -1 -c:a pcm_f32le "%%~nj".wav
+        del "..\%%~nxj"
+        lame -q 0 -b 320 --cbr --replaygain-accurate "%%~nj".wav
+    )
     move *.mp3 "%userprofile%\desktop"
+    del *.wav
+    cd ..
 )
 cd %temp%
 rmdir /s /q ytd
+yt-dlp --rm-cache-dir
 popd
